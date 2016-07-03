9b3b11e9a46d0816ab4c9225d17fa60b141f28c5
Wun Chiou <wun@utdallas.edu>
2016-07-03 03:03:28 -0500

    Organize JS better. Disable lightbox and hover tips on mobile.

--- js/wunchiou.js
+++ js/wunchiou.js
@@ -1,53 +1,85 @@
 ﻿var navlocation = "#about";
 var navlist = [];
+var num = 0;
+var num_ajax_sections = 4;
+var sm_width = 480;
 
-jQuery(document).ready(function($) {
+/**
+ * Show the current page location in the nav
+ * @param  {string} loc : the CSS selector for the current location
+ */
+function setlocation(loc) {
+	navlocation = loc;
+	$('nav > a[href!='+loc+']').removeClass('nav_selected');
+	$('nav > a[href='+loc+']').addClass('nav_selected');
+}
 
-	// ---------------------------------------
-	// Set current nav state
-	// ---------------------------------------
-
-	function setlocation(loc) {
-		navlocation = loc;
-		$('nav > a[href!='+loc+']').removeClass('nav_selected');
-		$('nav > a[href='+loc+']').addClass('nav_selected');
+/**
+ * Register the hover tips.
+ * @param  {jQuery} $items  : items to register
+ * @param  {Object} options : TipTip options
+ */
+function registerHoverTips($items, options) {
+	if (window.innerWidth > sm_width) {
+		$items.tipTip(options);
 	}
+}
 
-	// ---------------------------------------
-	// Smooth scrolling for same-page anchors
-	// ---------------------------------------
+/**
+ * Register images to load in the lightbox
+ * @param  {jQuery} $items
+ * @param  {function} linkmapper
+ */
+function registerLightbox($items, linkmapper) {
+	if (window.innerWidth > sm_width) {
+		var linkmapper = linkmapper || function(el) {
+			return [el.href, (el.title || el.firstChild.alt)];
+		};
 
-	$('a[href*=#]').click(
-	function(e) {
-		e.preventDefault();
-		var target = $(this).attr("href");
-		$('footer').css('height', '200px'); // Mobile Safari hack, part 1
+		$items.parent().slimbox({
+			resizeDuration: 200,
+			imageFadeDuration: 200,
+			captionAnimationDuration: 100,
+		}, linkmapper);
+	}
+}
 
-		// perform animated scrolling by getting top-position of target-element and set it as scroll target
-		$('html, body').stop().animate({ scrollTop: $(target).offset().top }, 700, 'swing',
-			function() {
-		    	location.hash = target;  //attach the hash (#jumptarget) to the pageurl
-		    	$('footer').css('height', '1px'); // Mobile Safari hack, part 2
-			});
+/**
+ * After last ajax is loaded: Set nav location on page scroll, register hover tips & lightbox links
+ */
+function ajaxloaded () {
+	if (++num >= num_ajax_sections) {
+		// build an array of sections on the page
+		$('section').each(function() {
+			navlist.push('#' + this.id);
+		});
 
-		return false;
-	});
+		// set the waypoints
+		$('section').waypoint(function(event, direction) {
+			if (direction === 'down') {
+				setlocation('#'+this.id);
+			} else {
+				setlocation( navlist[navlist.indexOf('#'+this.id)-1] );
+			}
+		}, {
+	   		offset: '50%'  // middle of the page
+		});
 
-   	// ---------------------------------------
-   	// Load Resumé
-   	// ---------------------------------------
-   	$('#resume_content').load('resume/resume.html', ajaxloaded);
+		// Register work image links with lightbox
+		$('#work_content article').each(function() {
+			registerLightbox($(this).find('a > img'));
+		});
 
-   	// ---------------------------------------
-   	// Load Work Portfolio
-   	// ---------------------------------------
-   	$('#work_content').load('portfolio/portfolio.html', ajaxloaded);
+		// Register resume icons hover tips
+		registerHoverTips($('#resume_content a[title]'), {defaultPosition:"top",edgeOffset:20});
+	}
+}
 
-   	// ---------------------------------------
-   	// Load posts from Wordpress
-   	// ---------------------------------------
-
-   	$('#post_content').html('Loading posts from Wordpress ...');
+/**
+ * Load the blog posts from Wordpress
+ */
+function loadPostsSection() {
+	$('#post_content').html('Loading posts from Wordpress ...');
 	$.getJSON("https://public-api.wordpress.com/rest/v1/sites/wunchiou.wordpress.com/posts/?number=5&callback=?",
 	function(json) {
 		if (json !== "error"){
@@ -67,25 +99,22 @@ jQuery(document).ready(function($) {
 				$(article).prepend(header);
 				$('#post_content').append(article);
 			});
-			// Register post image links with Slimbox2
-			//$('article a > img:not([data-orig-file])').parent().slimbox();
-			$('#post_content article a > img:not([data-orig-file])').parent().slimbox({},function(el) {
+			registerLightbox($('#post_content article a > img:not([data-orig-file])'),function(el) {
 				return [el.href.replace(/\?.+/, "") + '?h=600', (el.title || el.firstChild.alt)];
-				});
-			$('#post_content article a > img[data-orig-file]').parent().slimbox({},function(el) {
-				return [el.firstChild.getAttribute('data-orig-file')+'?h=600',
-					(el.title || el.firstChild.alt)];
-				});
-			
+			});
+			registerLightbox($('#post_content article a > img[data-orig-file]'),function(el) {
+				return [el.firstChild.getAttribute('data-orig-file')+'?h=600', (el.title || el.firstChild.alt)];
+			});	
 		} else {
 			$('#post_content').html("Sorry, cannot load posts right now.");
 		}
 	}).always(ajaxloaded);
+}
 
-   	// ---------------------------------------
-   	// Load photos from Flickr
-   	// ---------------------------------------
-	
+/**
+ * Load the pictures section from Flickr
+ */
+function loadPicturesSection() {
 	var flickr_url = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos"
 						+ "&api_key=a26d129b372bb3273bfa954be5200d2c"
 						+ "&user_id=49503158742@N01"
@@ -102,52 +131,41 @@ jQuery(document).ready(function($) {
 					.append($('<a>',{href:photo.url_z,title:photo.title})
 						.append( $('<img>',{src:photo.url_q,alt:photo.title})));
 			});
-			// Register Flickr image links with Slimbox2
-			$('#pictures_content a > img').parent().slimbox({
-				resizeDuration:200,imageFadeDuration:200,captionAnimationDuration:100},
-				function(el) {
-					return [el.href, (el.title || el.firstChild.alt)];
-				});
-			// Register Flickr image links with tipTip
-			$('#pictures_content a > img').parent().tipTip({defaultPosition:"top",edgeOffset:150});
+			registerLightbox($('#pictures_content a > img'));
+			registerHoverTips($('#pictures_content a > img').parent(), {defaultPosition:"top",edgeOffset:150});
 		} else {
 			console.log(json.stat);
 			$('#pictures_content').html("Sorry, cannot load pictures right now.");
 		}
 	}).always(ajaxloaded);
+}
 
-   	// ---------------------------------------
-   	// Set contact hover tip effect
-   	// ---------------------------------------
-	$('#contact a[title]').tipTip({edgeOffset:15});
+/**
+ * Things to do when the page is ready
+ */
+jQuery(document).ready(function($) {
 
-   	// ---------------------------------------
-   	// After ajax is loaded: Set nav location on page scroll, register Slimbox links
-   	// ---------------------------------------
-   	var num = 0;
-   	function ajaxloaded () {
-   		if (++num >= 4) {		// do this after the last ajax section is loaded
-   			// build an array of sections on the page
-   			$('section').each(function(){
-				navlist.push('#' + this.id);
+	// Smooth scrolling for same-page anchors
+	$('a[href*=#]').click(
+	function(e) {
+		e.preventDefault();
+		var target = $(this).attr("href");
+		$('footer').css('height', '200px'); // Mobile Safari hack, part 1
+
+		// perform animated scrolling by getting top-position of target-element and set it as scroll target
+		$('html, body').stop().animate({ scrollTop: $(target).offset().top }, 700, 'swing',
+			function() {
+		    	location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+		    	$('footer').css('height', '1px'); // Mobile Safari hack, part 2
 			});
-   			// set the waypoints
-			$('section').waypoint(function(event, direction) {
-				if (direction === 'down') {setlocation('#'+this.id);}
-				else {setlocation( navlist[navlist.indexOf('#'+this.id)-1] );}
-			}, {
-		   		offset: '50%'  // middle of the page
-			});
-			// Register  image links with Slimbox2
-			$('#work_content article').each(function() {
-				$(this).find('a > img').parent().slimbox({
-					resizeDuration:200,imageFadeDuration:200,captionAnimationDuration:100},
-					function(el) {
-						return [el.href, (el.title || el.firstChild.alt)];
-				});
-			});
-			// Register tipTip icons for resume icons
-			$('#resume_content a[title]').tipTip({defaultPosition:"top",edgeOffset:20});
-   		}
-	}
+
+		return false;
+	});
+
+   	$('#resume_content').load('resume/resume.html', ajaxloaded);
+   	$('#work_content').load('portfolio/portfolio.html', ajaxloaded);
+   	loadPostsSection();
+   	loadPicturesSection();
+	registerHoverTips($('#contact a[title]'), {edgeOffset:15});
+
 });
