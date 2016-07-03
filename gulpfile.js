2aa59211efb465b4d3a7db724b8993845e05c697
Wun Chiou <wun@utdallas.edu>
2016-07-02 22:31:48 -0500

    Use autoprefixer instead of manual prefixes.
    Autoprefixer is nice :)

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,26 @@ var paths = {
 	css: 'css',
 };
 
+var options = {
+	sass: {
+		outputStyle: 'compressed',
+	},
+	autoprefixer: {
+		browsers: ['> 1%'],
+		cascade: false,
+	}
+};
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
 	return gulp.src(paths.sass + '/style.scss')
 		.pipe(sourcemaps.init())
-		.pipe(sass({
-				outputStyle: 'compressed',
-			})
-			.on('error', function (err) {
-				console.error('Error!', err.message);
-			})
-		)
+		.pipe(sass(options.sass).on('error', sass.logError))
+		.pipe(autoprefixer(options.autoprefixer))
 		.pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(paths.css));
 });
